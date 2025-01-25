07efbf3d8e88dbd1db97e95036b2b6ff1d419a15
orestulis05 <orestaspetkus@gmail.com>
2025-01-25 04:44:28 +0200

    New and improved movement
    -Diagonal movement and shooting
    - Colors <3
    - Room camera
    - Refactorings on basically everything

--- Player.gd
+++ Player.gd
@@ -9,67 +9,94 @@ extends CharacterBody2D
 @onready var initial_scale : Vector2 = scale
 @export var bubble_decrease_rate : float = 0.5
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const BULLET_SPEED = 200.0
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+@export var bullet_speed = 200.0
 
 func _ready() -> void:
 	shooting_collider.disabled = true
 
 func _physics_process(delta: float) -> void:
+	_movement()
+	_shooting()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	_movement()
-	_shooting()
-
+	
+	# Change color
+	if bubble_quantity <= 50:
+		$Rect.modulate = Color.RED
+	if bubble_quantity <= 25:
+		get_tree().reload_current_scene()
+	
 	move_and_slide()
 	
 func _movement():
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 		
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		
 func _shooting():
 	var shooting_input : Vector2 = Vector2(Input.get_axis("shoot_left", "shoot_right"), Input.get_axis("shoot_up", "shoot_down"))
-	
+
 	if shooting_input.length() != 0:
+		shooting_area_pivot.rotation_degrees = _calculated_shooting_angle(shooting_input)
 		shooting_collider.disabled = false
-		if shooting_input == Vector2.RIGHT:
-			shooting_area_pivot.rotation_degrees = 0
-		if shooting_input == Vector2.DOWN:
-			shooting_area_pivot.rotation_degrees = 90
-		if shooting_input == Vector2.LEFT:
-			shooting_area_pivot.rotation_degrees = 180
-		if shooting_input == Vector2.UP:
-			shooting_area_pivot.rotation_degrees = 270
 		bubble_quantity -= bubble_decrease_rate
-		#print(bubble_quantity)
 		scale = _calculated_player_scale()
 	else:
 		shooting_collider.disabled = true
 	_shooting_movement(shooting_input)	
-		
 
 func _shooting_movement(shooting_input):
-	if shooting_input == Vector2.RIGHT:
-		velocity.x += -SPEED
-	if shooting_input == Vector2.LEFT:
-		velocity.x += SPEED
-	if shooting_input == Vector2.DOWN:
-		velocity.y = JUMP_VELOCITY
-	if shooting_input == Vector2.UP:
-		velocity.y = -JUMP_VELOCITY
+	if shooting_input.length() > 0:
+		if shooting_input.y > 0:
+			velocity.y = shooting_input.y * -speed
+		elif shooting_input.y < 0:
+			velocity.y += shooting_input.y * -speed * 0.3
+		velocity.x += shooting_input.x * -speed
 
 func _calculated_player_scale() -> Vector2:
 	var scaleMult : float = bubble_quantity / max_bubble_quantity
 	var result = initial_scale * scaleMult
 	return result
 
+func _calculated_shooting_angle(input : Vector2) -> float:
+	var dot : float
+	var cosine : float
+	var radians : float
+	var degrees : float
+	
+	if input.y > 0:
+		dot = input.dot(Vector2.RIGHT)
+		cosine = dot / (input.normalized().length() * Vector2.RIGHT.length())
+		radians = acos(cosine)
+		degrees = rad_to_deg(radians)
+	elif input.y < 0:
+		dot = input.dot(Vector2.LEFT)
+		cosine = dot / (input.normalized().length() * Vector2.LEFT.length())
+		radians = acos(cosine)
+		degrees = rad_to_deg(radians) + 180.0
+	else:
+		if input.x > 0:
+			degrees = 0
+		elif input.x < 0:
+			degrees = 180
+	
+	if (input.x == 1 and input.y == 1):
+		degrees = 45
+	elif (input.x == 1 and input.y == -1):
+		degrees = 315
+	elif (input.x == -1 and input.y == 1):
+		degrees = 135
+	elif (input.x == -1 and input.y == -1):
+		degrees = 225
+	
+	return degrees
